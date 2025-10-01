ac23cdb66befb13071ef502b1d26ae1ed9521c13
Yuki Shimizu <yuk.shimiz@gmail.com>
2025-10-01 10:32:40 +0900

    Initial Commit

---
+++ Stripe to camera/Stripe_to_cameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stripe_to_cameraApp.swift
+//  Stripe to camera
+//
+//  Created by yuk.shimizu on 2025/10/01.
+//
+
+import SwiftUI
+
+@main
+struct Stripe_to_cameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
